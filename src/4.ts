953ff6338d8d93a6c48f3545524f5a4ad4b2cd83
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-11-27 16:02:37 +0200

    Done 3 task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,22 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature;
+    }
+};
+
+class Person {
+    constructor(private key: Key) { };
+
+    getKey(): Key {
+        return this.key;
+    }
+};
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
